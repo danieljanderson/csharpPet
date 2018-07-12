48b24ea81f13a0695b553b29822dc50c37ae6e40
Daniel <danieljanderson0616@gmail.com>
2018-07-12 15:37:17 -0400

    started to work on the pet object

--- pet/Program.cs
+++ pet/Program.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿// things i have learned translating this app from javascript to c#
+// learned public functions anyone can call the methods.
+// protected only classes can call those methods.  so i would need to call generatedRandomNumber inside the class pet. 
+using System;
 
 namespace pet
 {
     class Program
-    {
+    { 
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
+    }
+    class Pet{
+        private string name;
+        private int food = 10;
+        private int energy = 10;
+        private int happiness = 10;
+        protected static int generateRandomNumber(int min, int max){
+            //Random is a object in the System class and I am making a new instance of it here.
+            Random rnd =new Random();
+            int randomNumber = rnd.Next(min,max);
+            return randomNumber;
+        }
+       
     }
 }
